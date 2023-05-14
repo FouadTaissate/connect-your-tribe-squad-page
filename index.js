beded4b5ecb3ce3660a36ddc1ce9dbe1b3d9f8d6
FouadTaissate <fouad.taissate@hva.nl>
2023-05-14 03:08:05 +0200

    finish

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
 import express from "express";
 
+const url = "https://whois.fdnd.nl/api/v1/squad/";
 
-// Haalt de API gegevens op
-const url = 'https://whois.fdnd.nl/api/v1/members?first=200'
-//  Haalt alle data op en wacht voordat voorgaande functions worden uitgevoerd
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error)
-
-// Haalt de API gegevens op
-const urlA = 'https://whois.fdnd.nl/api/v1/squad/squad-a-2022'
-//  Haalt alle data op en wacht voordat voorgaande functions worden uitgevoer
-const dataA = await fetch(urlA)
-  .then((response) => response.json())
-  .catch((error) => error)
-
-// API met alle studenten van Squad B
-const urlB= 'https://whois.fdnd.nl/api/v1/squad/squad-b-2022'
-const dataB = await fetch(urlB)
-  .then((response) => response.json())
-  .catch((error) => error)
-
-// API met alle studenten van Squat C
-const urlC = 'https://whois.fdnd.nl/api/v1/squad/squat-c-2022'
-const dataC = await fetch(urlC)
-  .then((response) => response.json())
-  .catch((error) => error)
+const data = await fetch(url).then((response) => response.json());
 
 
 
-console.log(data);
+
 
 // Maak een nieuwe express app
 const app = express();
@@ -40,46 +17,57 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get("/", (request, response) => {
-  console.log(request.query.squad);
+app.get("/", (req, res) => {
+  console.log(req.query.squad);
 
-  response.render("index", data);
+  let slug = req.query.squad || 'squat-c-2022'
+  let orderBy = req.query.orderBy || 'name'
+  let direction
+
+  
+  let role = req.query.role
+  
+  switch (orderBy) {
+    case "name-AZ":
+        orderBy = "name"
+        direction = "ASC"
+        break;
+    case "name-ZA":
+        orderBy = "name"
+        direction = "DESC"
+        break;
+    case "surname-AZ":
+        orderBy = "surname"
+        direction = "ASC"
+        break;
+    case "surname-ZA":
+        orderBy = "surname"
+        direction = "DESC"
+        break;
+    case "nickname-AZ":
+        orderBy = "nickname"
+        direction = "ASC"
+        break;
+    case "nickname-ZA":
+        orderBy = "nickname"
+        direction = "DESC"
+        break;                                
+    
+}
+
+let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=' + direction;
+// let squadUrl = `${url}${slug}?orderBy=orderBy&direction=${direction}`;
+
+
+console.log(squadUrl)
+
+
+fetchJson(squadUrl).then((data) => {
+  res.render('index', data)
+})
 });
 
-app.get('/squad-A', function (req, res) {
 
-  let slug = req.query.squad || 'squad-a-2022'
-  let orderBy = req.query.orderBy || 'name' + '&direction=ASC'
-  let squadUrl = urlA + '?orderBy=' + orderBy + '&direction=ASC'
-
-  fetchJson(squadUrl).then((dataA) => {
-    res.render('squad-A', dataA)
-  })
-})
-
-// Maakt een route voor de squad B pagina
-app.get('/squadB', function (req, res) {
-
-  let slug = req.query.squad || 'squad-a-2022'
-  let orderBy = req.query.orderBy || 'name' + '&direction=ASC'
-  let squadUrl = urlB + '?orderBy=' + orderBy + '&direction=ASC'
-
-  fetchJson(squadUrl).then((dataB) => {
-    res.render('squadB', dataB)
-  })
-})
-
-// Maakt een route voor de squat C pagina
-app.get('/squat-C', function (req, res) {
-
-  let slug = req.query.squad || 'squad-a-2022'
-  let orderBy = req.query.orderBy || 'name' + '&direction=ASC'
-  let squadUrl = urlC + '?orderBy=' + orderBy + '&direction=ASC'
-
-  fetchJson(squadUrl).then((dataC) => {
-    res.render('squatC', dataC)
-  })
-})
 
 // Stel het poortnummer in en start express
 app.set("port", process.env.PORT || 8000);
@@ -91,4 +79,4 @@ async function fetchJson(url) {
   return await fetch(url)
     .then((response) => response.json())
     .catch((error) => error)
-}
+};
